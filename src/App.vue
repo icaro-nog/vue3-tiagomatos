c9d575430ce4f06a7af1354c08c175ae6e11ef12
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-07-16 20:59:42 -0300

    primeiro componente TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import TheWelcome from './components/TheWelcome.vue'
+  import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+  <!-- <header>
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+    
+    
+  </header> -->
+  <TheHeader></TheHeader>
+  
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
   <main>
     <TheWelcome />
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script>
+    
+</script>
+
+<template>
+    <header class="header">
+        header
+    </header>
+</template>
+
+<style>
+    .header{
+        background: red;
+        color: azure
+    }
+</style>
