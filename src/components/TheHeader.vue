2555b1db29267a9f68690a6819b030a47aa8208e
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-07-17 15:35:33 -0300

    diretiva v-for e v-bind:key

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,67 @@
-<script>
+<script setup>
+    import { onMounted, ref } from 'vue';
     
+    const todos = ref([
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        }
+    ]);
+
+    onMounted(() => {
+        console.log(todos.value[0].title)
+    })
+
 </script>
 
 <template>
-    <header class="header">
-        header
-    </header>
+    <div>
+        <div 
+            v-for="(obj, index) in todos"
+            v-bind:key="obj.id"
+            class="todos-item"
+        >
+            {{ index + ' - ' + obj.title }}
+        </div>
+    </div>
 </template>
 
 <style>
     .header{
         background: red;
         color: azure
+    }
+
+    .todos-item{
+        margin: 0 0 5px 0;
+        padding: 3px;
+        color: black;
+        background: greenyellow;
     }
 </style>
