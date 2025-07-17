34561206b8434351dc91b214c6e4ff266241f065
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-07-17 16:41:09 -0300

    atributos class e style dinâmicos

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -39,6 +39,11 @@
     const imgAlt = ref('Imagem de Jon Snow')
     const imgSrc = ref('https://placehold.co/150x150')
 
+    const classConst = ref('title')
+    const isHome = ref(true)
+    const pClass = ref(['text', { 'text-home': isHome }])
+    const styleClass = ref({ 'color': 'darkorange', backgroundColor: 'green'});
+
     onMounted(() => {
         console.log(todos.value[0].title)
     })
@@ -47,6 +52,21 @@
 
 <template>
     <div>
+
+        <h1 
+            v-bind:class="{ 'title': true, 'title-home': isHome }"
+        >
+            Curso Vue 3 Tiago Matos
+        </h1>
+
+        <p v-bind:class="pClass">
+            Mussum Ipsum, cacilds vidis litro abertis
+        </p>
+
+        <p v-bind:style="styleClass">
+            Mussum Ipsum, cacilds vidis litro abertis
+        </p>
+
         <div 
             v-for="(obj, index) in todos"
             v-bind:key="obj.id"
@@ -73,5 +93,23 @@
         padding: 3px;
         color: black;
         background: greenyellow;
+    }
+
+    .title{
+        font-size: 20px;
+        color: blue;
+    }
+
+    .title-home{
+        font-size: 40px;
+        color: aqua;
+    }
+
+    .text{
+        color: crimson;
+    }
+
+    .text-home {
+        color: darkorchid;
     }
 </style>
