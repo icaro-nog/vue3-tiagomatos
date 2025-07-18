dd60662e0543365ee2cb8f2b0b04631aaca62655
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-07-18 14:06:05 -0300

    Eventos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import HelloWorld from './components/HelloWorld.vue'
   import TheHeader from './components/TheHeader.vue'
   import TheBidirecional from './components/TheBidirecional.vue'
+  import Eventos from './components/Eventos.vue'
   import { ref } from 'vue'
 
   const showHeader = ref(true);
@@ -30,7 +31,8 @@
   <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
   <main>
-    <TheBidirecional />
+    <!-- <TheBidirecional /> -->
+    <Eventos />
   </main>
 </template>
 
---
+++ src/components/Eventos.vue
@@ -0,0 +1,56 @@
+<script setup>
+    function onClick($evt){
+        console.log('click', $evt)
+    }
+
+    function onMouseover($evt){
+        console.log('mouse over', $evt)
+    }
+
+    function onMouseout($evt){
+        console.log('mouse out', $evt)
+    }
+
+    function onSubmit($evt){
+        console.log('submit', $evt)
+    }
+
+    function onKeyup($evt){
+        console.log('keyup', $evt)
+    }
+</script>
+
+<template>
+    <div>
+        <button @click.once="onClick">
+            Enviar
+        </button>
+        <br>
+        <div 
+            @mouseover="onMouseover"
+            @mouseout="onMouseout"
+        >
+            Mouse over
+        </div>
+
+        <br><br>
+
+        <form 
+            action="https://google.com"
+            @submit.prevent="onSubmit"
+        >
+            <input 
+                type="text"
+                @keyup="onKeyup"
+            >
+            <button type="submit">
+                Enviar
+            </button>
+        </form>
+
+    </div>
+</template>
+
+<style>
+
+</style>
